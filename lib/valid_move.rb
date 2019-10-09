8ce7e854422acc355b6a5fde6f0cfe0017eb303b
Mark Kauffman <markkauffman22@gmail.com>
2019-10-09 15:49:22 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,20 @@
 # code your #valid_move? method here
-
+def valid_move?
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, i)
+  if board.length != 0 
+    puts board[i]
+  end
+  if board.length == 0 
+    puts "ZERO LENGTH"
+    return false
+  elsif (board[i] == "X") || (board[i] == "O")
+    puts "true"
+    return true
+  else
+    puts false
+    return false
+  end  
+end
+
