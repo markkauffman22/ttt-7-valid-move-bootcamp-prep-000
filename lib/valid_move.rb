1d38899657f8f2f51dfb7baf52a2d7d0cc29a49e
Mark Kauffman <markkauffman22@gmail.com>
2019-10-09 15:57:09 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,13 @@
 # code your #valid_move? method here
-=begin
-1. You must move to a position within the tic-tac-toe board.
+# 1. You must move to a position within the tic-tac-toe board.
+# 2. The position must be vacant, not currently taken by a player.
+def valid_move?(position)
+  # firt check if position taken
+  if (position_taken?(board, i) == true) ||
+  
+end  
 
-The position must be vacant, not currently taken by a player.
-def valid_move?
 
-end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, i)
